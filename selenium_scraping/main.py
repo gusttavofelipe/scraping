38e5014dab27de81dbbaf91b891115cd6a6f589b
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-11-16 22:13:34 -0300

    selecionar disciplinas não concluidas

--- selenium_scraping/main.py
+++ selenium_scraping/main.py
@@ -55,15 +55,14 @@ class Selenium:
             print('error to find elements:', e)
 
 
-    def find_element(self, by: By, selector):
-        sleep(10)
+    def find_element(self, by: By, selector, time=10):
+        sleep(time)
         try:
             return self.driver.find_element(by, selector)
         except Exception as e:
-            print('error to find elements:', e)
+            print('error to find element:', e)
 
-
-    def send_keys(self, keys, time=4):
+    def send_keys(self, keys, time=1):
         sleep(time)
         try:
             self.element.send_keys(keys)
@@ -79,7 +78,7 @@ class Selenium:
             print('error to press enter:', e)
     
 
-    def click_element(self, by: By, selector, time=2, aftertime=0):
+    def click_element(self, by: By, selector, time=1, aftertime=0):
         sleep(time)
         try:
             self.driver.find_element(
@@ -94,19 +93,46 @@ class Selenium:
 if __name__ == '__main__':
     driver = Selenium()
 
-    # entrar na sala
+    # ENTRAR NA SALA
+    # entra na pagina de login
     driver.access('https://estudante.estacio.br/disciplinas')
+
+    # clica em entrar na pagina inicial de login
     driver.click_element(
         By.XPATH, '//*[@id="section-login"]/div/div/div[1]/section/div[1]/button', 3
         )
+    # seleciona input de email
     driver.select_element(By.ID, 'i0116')
+    # insere o email
     driver.send_keys(env.EMAIL)
+    # da enter depois que insere o email
     driver.enter()
+    # seleciona input de senha
     driver.select_element(By.ID, 'i0118')
-    driver.send_keys(env.PASSWORD, 5)
+    # insere a senha
+    driver.send_keys(env.PASSWORD)
+    # da enter depois que insere a senha
     driver.enter()
+    # clica em confirmar para avançar o login
     driver.click_element(By.ID, 'idSIButton9')
+
+    # clica em não mostrar de novo no aviso
     driver.click_element(By.ID, 'KmsiCheckboxField')
+    # clica em yes/sim para entrar na sala
     driver.click_element(By.ID, 'idSIButton9')
+
+    # ACESSAR DISCIPLINA
+    # pega a section que contem todos os cards de disciplina
+    disciplines_parent = driver.find_element(By.XPATH,'//*[@id="app"]/section')
+    # pega todos os elementos que indicam o progresso da disciplina
+    progress_bar = disciplines_parent.find_elements(By.CLASS_NAME, 'css-1wp63ts')
+    # EM ANDAMENTO - pega todos os botões de abrir a disciplina 
+    disciplines = disciplines_parent.find_elements(By.ID, 'btn-acessar-disciplina')
+    
+    # encontra disciplinas que não estão completas
+    for index in range(len(progress_bar)):
+        if '%' in progress_bar[index].text:
+            if '100%' not in progress_bar[index].text:
+                print(f'text = {progress_bar[index].text} --- index = {index}')
 
     logger.info('Scraping finalizado')
